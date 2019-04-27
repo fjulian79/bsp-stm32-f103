4666532e750cf3f2d71f570551598dccf327c8a3
Julian Friedrich <julian.friedrich@gmx.at>
2019-04-27 22:55:00 +0200

    Added GPL v3 license

--- bsp.cpp
+++ bsp.cpp
@@ -1,9 +1,25 @@
 /*
- * bsp.cpp
+ * bsp-samrtsink, the board support package for the hardware used in the 
+ * smartsink project.
  *
- *  Created on: Mar 25, 2018
- *      Author: julian
+ * Copyright (C) 2019 Julian Friedrich
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>. 
+ *
+ * You can file issues at https://github.com/fjulian79/bsp-smartsink
  */
+
 
 #include <stm32f1xx_ll_rcc.h>
 #include <stm32f1xx_ll_system.h>
